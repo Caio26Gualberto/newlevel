37a0378f80cdb81da86e7b1f3c8e53486ded9364
Caio26Gualberto <caiogualbertodev@outlook.com>
2024-04-04 02:04:29 -0300

    Adicionando fotos que usarei posteriormente e configurando Rota basica para pagina de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Routes from './routes/Rotas';
+
 
 function App() {
   return (
-    <div className="App">
-      caio
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
---
+++ frontend/src/routes/Rotas.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+import Login from '../views/loginAndRegister/Login';
+
+const Rotas = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" element={<Login/>} />
+      </Switch>
+    </Router>
+  )
+}
+
+export default Rotas
